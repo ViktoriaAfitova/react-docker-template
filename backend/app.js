f148bac00ec7f48627770b850b8b6f0f9789e7d1
Viktoria Afitova <ViktoriaAfitova@admins-MacBook-Pro.local>
2024-07-16 15:00:23 +0300

    Add routes

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,40 @@ const hostname = '0.0.0.0';
 const port = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
-  res.send('Hello Hello!');
+// routes
+app.get('/', async (req, res) => {
+  try {
+    const data = await db.query('SELECT * FROM schools');
+    res.status(200).send(data.rows);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  };
 });
 
-app.listen(port, hostname, () => {
+app.post('/', async (req, res) => {
+  const { name, location } = req.body;
+  try {
+    await db.query('INSERT INTO schools (name, address) VALUES ($1, $2)', [name, location]);
+    res.status(200).send({message: 'Successfully added child'});
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  };
+});
+
+app.get('/setup', async (req, res) => {
+  try {
+    await db.query('CREATE TABLE schools( id SERIAL PRIMARY KEY, name VARCHAR(100), address VARCHAR(100))');
+    res.status(200).send({message: 'Successfully created table'});
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  };
+});
+
+app.listen(port, hostname, async () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
