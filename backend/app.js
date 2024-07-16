1e3b11cb6ea0fbcdf3a997a745318aa2681740cb
Viktoria Afitova <ViktoriaAfitova@admins-MacBook-Pro.local>
2024-07-16 20:22:22 +0300

    Modify file structure

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,26 @@
 const express = require('express');
-const db = require('./db');
+const { Pool } = require('pg');
+
+const db  = new Pool({
+  host: 'db',
+  user: 'userTest',
+  password: 'passwordTest',
+  database: 'databaseTest'
+});
 
 const hostname = '0.0.0.0';
 const port = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // routes
-app.get('/', async (req, res) => {
+app.get('/setup', async (req, res) => {
   try {
-    const data = await db.query('SELECT * FROM schools');
-    res.status(200).send(data.rows);
+    await db.query('CREATE TABLE schools( id SERIAL PRIMARY KEY, name VARCHAR(100), address VARCHAR(100))');
+    res.status(200).send({message: 'Successfully created table'});
   } catch (error) {
-    console.log(error);
     res.sendStatus(500);
   };
 });
@@ -24,17 +31,15 @@ app.post('/', async (req, res) => {
     await db.query('INSERT INTO schools (name, address) VALUES ($1, $2)', [name, location]);
     res.status(200).send({message: 'Successfully added child'});
   } catch (error) {
-    console.log(error);
     res.sendStatus(500);
   };
 });
 
-app.get('/setup', async (req, res) => {
+app.get('/', async (req, res) => {
   try {
-    await db.query('CREATE TABLE schools( id SERIAL PRIMARY KEY, name VARCHAR(100), address VARCHAR(100))');
-    res.status(200).send({message: 'Successfully created table'});
+    const data = await db.query('SELECT * FROM schools');
+    res.status(200).send(data.rows);
   } catch (error) {
-    console.log(error);
     res.sendStatus(500);
   };
 });
