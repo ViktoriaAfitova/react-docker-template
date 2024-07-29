7991e839cf4aab8b3162495de5de10f5d0a7e4d6
Viktoria Afitova <ViktoriaAfitova@admins-MacBook-Pro.local>
2024-07-29 16:06:23 +0300

    Add run tests

---
+++ backend/test/test.js
@@ -0,0 +1,29 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const http = require('http');
+const { app } = require('../app');
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+describe('HTTP Server', () => {
+  let server;
+  before((done) => {
+    server = http.createServer(app);
+    server.listen(3001, done);
+  });
+
+  after((done) => {
+    done()
+  });
+
+  it('should return Server running', (done) => {
+    chai.request(server)
+      .get('/')
+      .end((err, res) => {
+        expect(res).to.have.st(200);
+        expect(res.text).to.e('Server running');
+      done();
+    });
+  });
+});
