35a4fbee16417360458080b363599abb91de9e58
Viktoria Afitova <ViktoriaAfitova@admins-MacBook-Pro.local>
2024-08-01 10:33:58 +0300

    Fix test action

--- backend/test/test.js
+++ backend/test/test.js
@@ -8,22 +8,35 @@ const expect = chai.expect;
 
 describe('HTTP Server', () => {
   let server;
+  const testPort = 3002;
+
   before((done) => {
     server = http.createServer(app);
-    server.listen(3001, done);
+    server.listen(testPort, (err) => {
+      if (err) {
+        console.error(`Failed to start server on port ${testPort}:`, err);
+        return done(err);
+      }
+      done();
+    });
   });
 
   after((done) => {
-    done()
+    if (server && server.listening) {
+      server.close(done);
+    } else {
+      done();
+    }
   });
 
-  it('should return Server running', (done) => {
+  it('should return "Server running"', (done) => {
     chai.request(server)
       .get('/')
       .end((err, res) => {
-        expect(res).to.have.status(200); 
-        expect(res.text).to.equal('Server running'); 
-      done();
-    });
+        if (err) return done(err);
+        expect(res).to.have.status(200);
+        expect(res.text).to.equal('Server running');
+        done();
+      });
   });
 });
